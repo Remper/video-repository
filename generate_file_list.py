e9d2255f6e526b067e01f6e8e452103a3dbb5e53
Yaroslav Nechaev <remper@me.com>
2018-01-21 21:30:14 +0100

    initial implementation

---
+++ generate_file_list.py
@@ -0,0 +1,17 @@
+import boto3
+import argparse
+import json
+
+parser = argparse.ArgumentParser(description='Basic deep neural network that works with SVM input')
+parser.add_argument('--bucket', required=True, help='Bucket name', metavar='#')
+args = parser.parse_args()
+
+s3 = boto3.resource('s3')
+
+videos = []
+for file in s3.Bucket(args.bucket).objects.all():
+    key = file.key
+    if key.lower().endswith('.mp4'):
+        videos.append(key)
+
+json.dump(videos, open('manifest.json', 'w'))
